defe143a12faa938a5ad53acfaec45b1e71bec82
shucan Yang <landmvrks@163.com>
2025-03-22 14:05:00 +0000

    initialize

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+const int maxm = 100;
+int main(void)
+{
+    char input[maxm];
+    while(1)
+    {
+        scanf("%s", input);
+        printf("%s\n", input);
+    }
+    return 0;
+}
