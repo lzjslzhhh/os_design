88b7a73858c7f9e3e5b40f7f9db286775bc15dbc
shucan Yang <landmvrks@163.com>
2025-03-27 08:24:13 +0000

    新增部分命令解析

--- src/shell.c
+++ src/shell.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
+#include<string.h>
 void start_shell(void)
 {
     char input[256];
     while(1)
     {
         scanf("%s", input);
+        if(strcmp(input, "ls") == 0)
+        {
+            //list all the files in this content
+        }
+        else if(strcmp(input, "mem") == 0)
+        {
+            //show memory status
+        }
         printf("%s\n", input);
     }
 }
